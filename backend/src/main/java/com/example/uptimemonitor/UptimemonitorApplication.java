becb5e1516b7c131cad59cfab5aa3333a6ecc6b4
Shreyansh <yourname@example.com>
2026-07-20 21:04:43 +0530

    initial commit

---
+++ backend/src/main/java/com/example/uptimemonitor/UptimemonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.uptimemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimemonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimemonitorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/uptimemonitor/UptimemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uptimemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimemonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
